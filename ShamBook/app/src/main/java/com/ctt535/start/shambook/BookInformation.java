a3c903ded0392874e4b71de933c3ba1fde4fc11a
big842 <nguoicodonvmd@gmail.com>
2016-07-30 12:30:17 +0700

    Change new project

---
+++ ShamBook/app/src/main/java/com/ctt535/start/shambook/BookInformation.java
@@ -0,0 +1,14 @@
+package com.ctt535.start.shambook;
+
+import android.graphics.Bitmap;
+
+public class BookInformation {
+    String title;
+    String authors;
+    String publishers;
+    String type;
+    String format;
+    Bitmap coverImage;
+    String filePath;
+    int precentRead;
+}
---
+++ ShamBook/app/src/main/java/com/ctt535/start/shambook/ListBookAdapter.java
@@ -0,0 +1,64 @@
+package com.ctt535.start.shambook;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ListBookAdapter extends ArrayAdapter<BookInformation> {
+    Activity context;
+    ArrayList<BookInformation> listBook;
+    int backgroundColor;
+    boolean showAllBooks;
+
+    public ListBookAdapter(Activity context, ArrayList<BookInformation> listBook, int backgroundColor, boolean showAllBooks) {
+        super(context, R.layout.list_book_layout, listBook);
+
+        this.context = context;
+        this.listBook = listBook;
+        this.backgroundColor = backgroundColor;
+        this.showAllBooks = showAllBooks;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.list_book_layout, null,true);
+
+        ImageView coverImage = (ImageView) rowView.findViewById(R.id.coverImage);
+        TextView bookTitle = (TextView) rowView.findViewById(R.id.bookTitle);
+        TextView bookAuthor = (TextView) rowView.findViewById(R.id.bookAuthor);
+        TextView percentRead = (TextView) rowView.findViewById(R.id.percentRead);
+        TextView bookFormat = (TextView) rowView.findViewById(R.id.bookFormat);
+        BookInformation bofo = listBook.get(position);
+
+        coverImage.setImageBitmap(bofo.coverImage);
+        bookTitle.setText(bofo.title);
+
+        String temp = "Format: " + bofo.format;
+        bookFormat.setText(temp);
+
+        if(!showAllBooks) {
+            temp = "Read: " + bofo.precentRead + "%";
+            percentRead.setText(temp);
+        }else{
+            percentRead.setText("");
+        }
+
+
+        try{
+            temp = "Authors: " + bofo.authors.substring(1, bofo.authors.length() - 1);
+        }catch (Exception ex){
+            temp = "Authors: Unknow";
+        }
+        bookAuthor.setText(temp);
+
+        rowView.setBackgroundColor(backgroundColor);
+        return rowView;
+    }
+}
